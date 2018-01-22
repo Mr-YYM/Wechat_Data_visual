de886f380d21ff8595214f82e85c43200a30c206
Yeming <492851378@qq.com>
2018-01-23 01:45:27 +0800

    fruter improvement the plot

--- demo.py
+++ demo.py
@@ -23,20 +23,32 @@ def insert_sort(v_lists, k_lists):
     return v_lists, k_lists
 
 
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct * total / 100.0))
+        return '{p:.1f}%({v:d})'.format(p=pct, v=val)
+
+    return my_autopct
+
+
 # implement a pie chart
-def get_pie(a_plt, pie_data, labels=None, title=None):
+def get_pie(a_plt, data, title=None):
     a_plt.clear()
     a_plt.axis("equal")
-    a_plt.pie(pie_data, labels=labels, autopct='%1.1f%%')
+    pie_data = list(data.values())
+    pie_labels = list(data.keys())
+    a_plt.pie(pie_data, radius=1.25, labels=pie_labels,
+              autopct=make_autopct(pie_data), pctdistance=0.65)
     a_plt.legend(loc='best')
     a_plt.set_title(title)
 
 
 # implement a bar chart
-def get_a_bar(a_plt, bar_data, category, title=None, if_sort=False):
+def get_a_bar(a_plt, bar_data, title=None, if_sort=False):
     a_plt.clear()
-    a_plt.set_xlabel(category)
-    a_plt.set_ylabel('数量')
+    a_plt.set_ylabel('数量', fontsize=20)
+    # a_plt.text(len(bar_data.keys()), -15, category, ha='center', va='bottom')  # x_label
     if if_sort:
         bar_values, bar_names = insert_sort(list(bar_data.values()), list(bar_data.keys()))
     else:
@@ -54,19 +66,22 @@ def get_a_bar(a_plt, bar_data, category, title=None, if_sort=False):
 def get_bar_data(p_data, category):
     fore_num, other_num, unknown_num = 0, 0, 0
     bar_data = {}
-    staring_num = 0.1 * max(p_data.values())  # 单独成为一个柱体的条件: >= 0.1
+    staring_num = 0.1 * max(p_data.values())  # 单独成为一个柱体的条件: >= 0.1*max_value
     for k, v in p_data.items():
-        if re.match('[\u4e00-\u9fa5]+',
-                    k) is not None and v >= staring_num:  # China(in Chinese char) province and eligible
+        if re.match('[\u4e00-\u9fa5]+', k) \
+                is not None and v >= staring_num:  # China(in Chinese char) province and eligible
             bar_data[k] = v
-        elif re.match('[\u4e00-\u9fa5]+', k) is not None:  # China(in Chinese char) province but ineligible
+        elif re.match('[\u4e00-\u9fa5]+', k) \
+                is not None:  # China(in Chinese char) province but ineligible
             other_num += v
         elif k == '':
             unknown_num += v
         else:
             fore_num += v  # foreign area
-    bar_data["其他%s" % category] = other_num
-    bar_data['国外'] = fore_num
+    if other_num != 0:
+        bar_data["其他\n%s" % category] = other_num
+    if fore_num != 0:
+        bar_data['国外'] = fore_num
     if unknown_num != 0:
         bar_data['unknown'] = unknown_num
     return bar_data
@@ -87,42 +102,47 @@ group.update_group(True)
 print(group.members.stats_text())
 group_members_stat = group.members.stats()
 
-# initial the plot
+# Initialize the plot
 mpl.style.use('seaborn')
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+mpl.rcParams['xtick.labelsize'] = 16
+mpl.rcParams['ytick.labelsize'] = 16
+mpl.rcParams['legend.fontsize'] = 16
+mpl.rcParams['axes.titlesize'] = 18
 plt.rcParams.update({'font.size': 15})
 
-# create a figure
+# generate a figure
 fig, axs = plt.subplots(3, 1, figsize=(9, 16), dpi=200)
 
 # Process the sex_data
 gms_of_sex = group_members_stat['sex']
 sex_num_dict = {'unknown': gms_of_sex[0], 'boy': gms_of_sex[1], 'girl': gms_of_sex[2]}
-sex_data = list(sex_num_dict.values())
-sex_labels = list(sex_num_dict.keys())
+sex_data = {k: v for k, v in sex_num_dict.items() if v != 0}  # filter out the element which its value is 0
 
 # show the sex_data as a pie
 pie_title = "该群({0})的{1}比例".format(group.name, '性别')
-get_pie(axs[0], sex_data, labels=sex_labels, title=pie_title)
+get_pie(axs[0], sex_data, title=pie_title)
 
 # Process the province_data (p:province)
 p_bar_data = get_bar_data(group_members_stat['province'], '省份')
 
 # show the province_data as a bar
 bar_title = "该群({0})的成员{1}分布".format(group.name, '省份')
-get_a_bar(axs[1], p_bar_data, '省份', title=bar_title, if_sort=True)
+get_a_bar(axs[1], p_bar_data, title=bar_title, if_sort=True)
 
 # Process the city_data
 c_bar_data = get_bar_data(group_members_stat['city'], '城市')
 
 # show the city_data as a bar
-bar_title = "该群({0})的成员{1}分布".format(group.name, '省份')
-get_a_bar(axs[2], c_bar_data, '城市', title=bar_title)
+bar_title = "该群({0})的成员{1}分布".format(group.name, '城市')
+get_a_bar(axs[2], c_bar_data, title=bar_title, if_sort=True)
 
 # show the plot
-fig.suptitle('群数据')
+fig.suptitle('群数据({0})'.format(group.name), fontsize=30)
+# Adjust layout to avoid the overlap between bar's label and the next bar's title
+fig.subplots_adjust(hspace=0.3)
 
-fig.savefig('fig.png')
+fig.savefig('fig.pdf')
 fig.show()
 # bot.logout()
